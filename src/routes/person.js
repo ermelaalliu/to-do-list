75fe68c6d8121211dcf52a8ddf4af8a536aa0b27
mela <alliuermela@gmail.com>
2023-04-17 13:41:00 +0200

    person querystring

--- src/routes/person.js
+++ src/routes/person.js
@@ -2,11 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 // QueryString => query property on the request object
+// localhost:3000/person?name=thomas&age=20
 router.get("/person", (req, res) => {
-    res.send("you have requested a person");
+
+    if(req.query.name) {
+        res.send(`you have requested a person ${req.query.name}`)
+    }
+    else{
+        res.send("you have requested a person");
+    }
 });
 
 // Params property on the request object
+// localhost: 3000/person/thomas
 router.get("/person/:name", (req, res) => {
     res.send(`you have requested a person ${req.params.name}`);
 });
